3bc40949da231937eb5fcc26f198b6d5f9761871
dolya-julia <juliadolya06@gmail.com>
2024-12-04 00:49:04 +0300

    add items

--- test.pl
+++ test.pl
@@ -1,6 +1,8 @@
 
 :- dynamic currentLocation/1.
 :- dynamic playerBalance/1.
+:- dynamic inventory/1.
+:- dynamic hasItem/2.
 
 connected(city, park, north).
 connected(city, work, south).
@@ -12,10 +14,21 @@ connected(cafe, park, south).
 
 currentLocation(city).
 playerBalance(0).
+inventory([]).
 
 job(work, job1, 50).
 job(work, job2, 30).
 job(cafe, job3, 20).
+
+hasItem(magazine, city).
+hasItem(flower, park).
+hasItem(documents, work).
+hasItem(coffee, cafe).
+
+itemValue(magazine, 10).
+itemValue(flower, 5).
+itemValue(documents, 20).
+itemValue(coffee, 15).
 
 move(_direction):-
     currentLocation(_current),
@@ -43,10 +56,38 @@ do_job(JobName):-
     format('Вы выполнили подработку ~w и заработали ~w монет!~n', [JobName, Reward]),
     format('Ваш новый баланс: ~w монет.~n', [NewBalance]).
 
+take(Item):-
+    currentLocation(Location),
+    hasItem(Item, Location),
+    itemValue(Item, Value),
+    retract(hasItem(Item, Location)),
+    inventory(Inv),
+    append(Inv, [Item], NewInv),
+    retract(inventory(Inv)),
+    assert(inventory(NewInv)),
+    format('Вы взяли ~w (ценность: ~d).~n', [Item, Value]).
 
+drop(Item):-
+    inventory(Inv),
+    member(Item, Inv),
+    select(Item, Inv, NewInv),
+    retract(inventory(Inv)),
+    assert(inventory(NewInv)),
+    currentLocation(Location),
+    assert(hasItem(Item, Location)),
+    format('Вы положили ~w обратно в ~w.~n', [Item, Location]).
 
+show_inventory :-
+    inventory(Inv),
+    format('Ваш инвентарь: ~w.~n', [Inv]).
 
+has_item(Item) :-
+    inventory(Inv),
+    member(Item, Inv).
 
+show_items(Location) :-
+    findall(Item, hasItem(Item,Location), Items),
+    format('В ~w находятся следующие предметы: ~w.~n', [Location, Items]).
 
 
 
