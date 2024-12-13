8a3c6185a08c4feedc4b26af874683d6a14e723c
polina_storozh <58592174+voird@users.noreply.github.com>
2024-12-14 01:48:49 +0300

    Update commands.clj

--- src/mire/commands.clj
+++ src/mire/commands.clj
@@ -19,6 +19,7 @@
        (str/join "\n" (map #(str "There is " % " here.\n")
                            @(:items @player/*current-room*)))))
 
+
 (defn move
   "\"♬ We gotta get out of this place... ♪\" Give a direction."
   [direction]
@@ -34,16 +35,43 @@
          (look))
        "You can't go that way."))))
 
+(def possible-items ["key" "sword" "map" "potion" "shield" "treasure chest" "scroll" "gemstone"])
+
+(def found-items (atom nil))  ;; Храним сгенерированный список, начальное значение - nil
+(def found-items-set (atom #{}))  ;; Множество найденных предметов
+
+(def game-over? (atom false))  ;; Переменная, указывающая на завершение игры
+
+
+(defn look-items
+  "Show the list of items the player needs to find. Generate it only once per session."
+  []
+  (if (nil? @found-items)  ;; Если список еще не сгенерирован
+    (let [items-to-find (take 3 (shuffle possible-items))]  ;; Случайно выбираем 3 предмета
+      (reset! found-items items-to-find)  ;; Сохраняем список в атом
+      (str "You need to find the following items:\n"
+           (str/join "\n" items-to-find)))  ;; Возвращаем строку с предметами
+    ;; Если список уже существует
+    (str "You need to find the following items:\n"
+         (str/join "\n" @found-items))))  ;; Просто возвращаем существующий список
+
+
 (defn grab
   "Pick something up."
   [thing]
   (dosync
    (if (rooms/room-contains? @player/*current-room* thing)
-     (do (move-between-refs (keyword thing)
-                            (:items @player/*current-room*)
-                            player/*inventory*)
-         (str "You picked up the " thing "."))
-     (str "There isn't any " thing " here."))))
+     (if (contains? @found-items (name thing))
+       (do
+         (swap! found-items-set conj (name thing))  ;; Добавляем предмет в найденные
+         (if (= (count @found-items-set) (count @found-items))  ;; Проверяем, все ли предметы найдены
+           (do
+             (reset! game-over? true)  ;; Завершаем игру
+             (str "Congratulations, " player/*name* ", you found all the items! You win!"))
+           (str "You picked up the " thing ".")))
+       (str "That is not part of the list of items you need to find."))  ;; Обработка неверного предмета
+     (str "There isn't any " thing " here."))))  ;; Обработка отсутствующего предмета
+
 
 (defn discard
   "Put something down that you're carrying."
@@ -53,24 +81,26 @@
      (do (move-between-refs (keyword thing)
                             player/*inventory*
                             (:items @player/*current-room*))
-         (str "You dropped the " thing "."))
-     (str "You're not carrying a " thing "."))))
+         (str "You dropped the " thing "."))  ;; Предмет был отброшен
+     (str "You're not carrying a " thing "."))))  ;; Предмет не найден в инвентаре
 
 (defn inventory
   "See what you've got."
   []
   (str "You are carrying:\n"
-       (str/join "\n" (seq @player/*inventory*))))
+       (str/join "\n" (seq @player/*inventory*))))  ;; Возвращает список предметов в инвентаре
 
 (defn detect
   "If you have the detector, you can see which room an item is in."
   [item]
   (if (@player/*inventory* :detector)
-    (if-let [room (first (filter #((:items %) (keyword item))
-                                 (vals @rooms/rooms)))]
-      (str item " is in " (:name room))
-      (str item " is not in any room."))
+    (let [room (first (filter #((:items %) (keyword item))
+                             (vals @rooms/rooms)))]
+      (if room
+        (str item " is in " (:name room))
+        (str item " is not in any room.")))
     "You need to be carrying the detector for that."))
+
 
 (defn say
   "Say something out loud so everyone in the room can hear."
@@ -86,45 +116,27 @@
 (defn help
   "Show available commands and what they do."
   []
-  (str/join "\n" (map #(str (key %) ": " (:doc (meta (val %))))
-                      (dissoc (ns-publics 'mire.commands)
-                              'execute 'commands))))
-
-(def possible-items ["key" "sword" "map" "potion" "shield" "treasure chest" "scroll" "gemstone"])
-
-(def found-items (atom nil))  ;; Храним сгенерированный список, начальное значение - nil
-
-(defn look-items
-  "Show the list of items the player needs to find. Generate it only once per session."
-  []
-  (if (nil? @found-items)  ;; Если список еще не сгенерирован
-    (let [items-to-find (take 3 (shuffle possible-items))]  ;; Случайно выбираем 3 предмета
-      (reset! found-items items-to-find)  ;; Сохраняем список в атом
-      (str "You need to find the following items:\n"
-           (str/join "\n" items-to-find)))  ;; Возвращаем строку с предметами
-    ;; Если список уже существует
-    (str "You need to find the following items:\n"
-         (str/join "\n" @found-items))))  ;; Просто возвращаем существующий список
+  (str/join "\n" (map #(str (key %) ": " (:doc (meta (val %)))))
+            (dissoc (ns-publics 'mire.commands)
+                    'execute 'commands)))  ;; Moved here
 
 
 ;; Command data
-
 (def commands {"move" move,
                "north" (fn [] (move :north)),
                "south" (fn [] (move :south)),
                "east" (fn [] (move :east)),
                "west" (fn [] (move :west)),
-               "grab" grab
-               "discard" discard
-               "inventory" inventory
-               "detect" detect
-               "look" look
-               "say" say
-               "help" help
+               "grab" grab,
+               "discard" discard,
+               "inventory" inventory,
+               "detect" detect,
+               "look" look,
+               "say" say,
+               "help" help,
                "look-items" look-items})
 
 ;; Command handling
-
 (defn execute
   "Execute a command that is passed to us."
   [input]
