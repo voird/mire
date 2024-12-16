fb4e0d6bc8ca1afb2706a48dcd7af7e5ff4fe17c
polina_storozh <58592174+voird@users.noreply.github.com>
2024-12-16 22:23:37 +0300

    Update commands.clj

--- src/mire/commands.clj
+++ src/mire/commands.clj
@@ -4,109 +4,117 @@
             [mire.player :as player]
             [clojure.set :as set]))
 
-(defn- move-between-refs
-  "Move one instance of obj between from and to. Must call in a transaction."
-  [obj from to]
-  (alter from disj obj)
-  (alter to conj obj))
+(defonce random-item (ref nil))  ; This will store the random item, initialized to nil.
+
+(defn random-item-to-find
+  "Generate a random item the player needs to find."
+  []
+  (dosync
+   (when (nil? @random-item)  ; If the random item has not been generated yet.
+     (ref-set random-item (rand-nth [:keys :detector :bunny :turtle])))  ; Randomly select one item.
+   (str "You need to find the " @random-item ".")))
+
+
+  (defn- move-between-refs
+    "Move one instance of obj between from and to. Must call in a transaction."
+    [obj from to]
+    (alter from disj obj)
+    (alter to conj obj))
 
 ;; Command functions
 
-(defn look
-  "Get a description of the surrounding environs and its contents."
-  []
-  (str (:desc @player/*current-room*)
-       "\nExits: " (keys @(:exits @player/*current-room*)) "\n"
-       (str/join "\n" (map #(str "There is " % " here.\n")
-                           @(:items @player/*current-room*)))))
+  (defn look
+    "Get a description of the surrounding environs and its contents."
+    []
+    (str (:desc @player/*current-room*)
+         "\nExits: " (keys @(:exits @player/*current-room*)) "\n"
+         (str/join "\n" (map #(str "There is " % " here.\n")
+                             @(:items @player/*current-room*)))))
 
-(defn move
-  "\"♬ We gotta get out of this place... ♪\" Give a direction."
-  [direction]
-  (dosync
-   (let [target-name ((:exits @player/*current-room*) (keyword direction))
-         target (@rooms/rooms target-name)]
-     (if target
-       (do
-         (move-between-refs player/*name*
-                            (:inhabitants @player/*current-room*)
-                            (:inhabitants target))
-         (ref-set player/*current-room* target)
-         (look))
-       "You can't go that way."))))
-
-
-(defn generate-random-items
-  "Генерирует случайный список предметов для игрока."
-  [available-items num-items]
-  (set (take num-items (shuffle available-items))))
-
-(defn initialize-player-items
-  "Инициализирует список предметов для игрока."
-  []
-  (let [available-items #{:keys :detector :bunny :turtle :map :flashlight}
-        num-items (+ (rand-int 3) 2) ; случайное количество предметов от 2 до 4
-        items (generate-random-items available-items num-items)]
+  (defn move
+    "\"♬ We gotta get out of this place... ♪\" Give a direction."
+    [direction]
     (dosync
-      (ref-set player/*inventory* items)
-      items)))
+     (let [target-name ((:exits @player/*current-room*) (keyword direction))
+           target (@rooms/rooms target-name)]
+       (if target
+         (do
+           (move-between-refs player/*name*
+                              (:inhabitants @player/*current-room*)
+                              (:inhabitants target))
+           (ref-set player/*current-room* target)
+           (look))
+         "You can't go that way."))))
 
 
-(defn grab
-  "Pick something up."
-  [thing]
-  (dosync
-   (if (rooms/room-contains? @player/*current-room* thing)
-     (do
-       (move-between-refs (keyword thing)
-                          (:items @player/*current-room*)
-                          player/*inventory*)
-       (let [total-items (initialize-player-items) ; инициализируем список всех предметов
-             all-items (deref player/*inventory*)] ; получаем предметы игрока
-         (if (= all-items total-items)
-           (str "You picked up the " thing ". You've collected all items! Game Over. Winner: " player/*name*)
-           (str "You picked up the " thing "."))))
-     (str "There isn't any " thing " here."))))
+  (defn generate-random-items
+    [available-items num-items]
+    (set (take num-items (shuffle available-items))))
+
+  (defn initialize-player-items
+    []
+    (let [available-items #{:keys :detector :bunny :turtle :map :flashlight}
+          num-items (+ (rand-int 3) 2) ; случайное количество предметов от 2 до 4
+          items (generate-random-items available-items num-items)]
+      (dosync
+       (ref-set player/*inventory* items)
+       items)))
 
 
-(defn discard
-  "Put something down that you're carrying."
-  [thing]
-  (dosync
-   (if (player/carrying? thing)
-     (do (move-between-refs (keyword thing)
-                            player/*inventory*
-                            (:items @player/*current-room*))
-         (str "You dropped the " thing "."))
-     (str "You're not carrying a " thing "."))))
+  (defn grab
+    "Pick something up."
+    [thing]
+    (dosync
+     (if (rooms/room-contains? @player/*current-room* thing)
+       (do
+         (move-between-refs (keyword thing)
+                            (:items @player/*current-room*)
+                            player/*inventory*)
+         (let [total-items (initialize-player-items) ; инициализируем список всех предметов
+               all-items (deref player/*inventory*)] ; получаем предметы игрока
+           (if (= all-items total-items)
+             (str "You picked up the " thing ". You've collected all items! Game Over. Winner: " player/*name*)
+             (str "You picked up the " thing "."))))
+       (str "There isn't any " thing " here."))))
 
-(defn inventory
-  "See what you've got."
-  []
-  (str "You are carrying:\n"
-       (str/join "\n" (seq @player/*inventory*))))
 
-(defn detect
-  "If you have the detector, you can see which room an item is in."
-  [item]
-  (if (@player/*inventory* :detector)
-    (if-let [room (first (filter #((:items %) (keyword item))
-                                 (vals @rooms/rooms)))]
-      (str item " is in " (:name room))
-      (str item " is not in any room."))
-    "You need to be carrying the detector for that."))
+  (defn discard
+    "Put something down that you're carrying."
+    [thing]
+    (dosync
+     (if (player/carrying? thing)
+       (do (move-between-refs (keyword thing)
+                              player/*inventory*
+                              (:items @player/*current-room*))
+           (str "You dropped the " thing "."))
+       (str "You're not carrying a " thing "."))))
 
-(defn say
-  "Say something out loud so everyone in the room can hear."
-  [& words]
-  (let [message (str/join " " words)]
-    (doseq [inhabitant (disj @(:inhabitants @player/*current-room*)
-                             player/*name*)]
-      (binding [*out* (player/streams inhabitant)]
-        (println message)
-        (println player/prompt)))
-    (str "You said " message)))
+  (defn inventory
+    "See what you've got."
+    []
+    (str "You are carrying:\n"
+         (str/join "\n" (seq @player/*inventory*))))
 
+  (defn detect
+    "If you have the detector, you can see which room an item is in."
+    [item]
+    (if (@player/*inventory* :detector)
+      (if-let [room (first (filter #((:items %) (keyword item))
+                                   (vals @rooms/rooms)))]
+        (str item " is in " (:name room))
+        (str item " is not in any room."))
+      "You need to be carrying the detector for that."))
+
+  (defn say
+    "Say something out loud so everyone in the room can hear."
+    [& words]
+    (let [message (str/join " " words)]
+      (doseq [inhabitant (disj @(:inhabitants @player/*current-room*)
+                               player/*name*)]
+        (binding [*out* (player/streams inhabitant)]
+          (println message)
+          (println player/prompt)))
+      (str "You said " message)))
 
 
 (defn help
@@ -116,28 +124,30 @@
                       (dissoc (ns-publics 'mire.commands)
                               'execute 'commands))))
 
+
 ;; Command data
 
-(def commands {"move" move,
-               "north" (fn [] (move :north)),
-               "south" (fn [] (move :south)),
-               "east" (fn [] (move :east)),
-               "west" (fn [] (move :west)),
-               "grab" grab
-               "discard" discard
-               "inventory" inventory
-               "detect" detect
-               "look" look
-               "say" say
-               "help" help})
+  (def commands {"move" move,
+                 "north" (fn [] (move :north)),
+                 "south" (fn [] (move :south)),
+                 "east" (fn [] (move :east)),
+                 "west" (fn [] (move :west)),
+                 "grab" grab
+                 "discard" discard
+                 "inventory" inventory
+                 "detect" detect
+                 "look" look
+                 "say" say
+                 "help" help
+                 "random-item" random-item-to-find})
 
 ;; Command handling
 
-(defn execute
-  "Execute a command that is passed to us."
-  [input]
-  (try (let [[command & args] (.split input " +")]
-         (apply (commands command) args))
-       (catch Exception e
-         (.printStackTrace e (new java.io.PrintWriter *err*))
-         "You can't do that!")))
+  (defn execute
+    "Execute a command that is passed to us."
+    [input]
+    (try (let [[command & args] (.split input " +")]
+           (apply (commands command) args))
+         (catch Exception e
+           (.printStackTrace e (new java.io.PrintWriter *err*))
+           "You can't do that!")))
